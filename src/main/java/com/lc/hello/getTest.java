a79c604ef9320aa8dd325aa465ed7f1271779f5b
lc <lc@qq.com>
2023-03-15 23:17:22 +0800

    first commit

---
+++ src/main/java/com/lc/hello/getTest.java
@@ -0,0 +1,12 @@
+package com.lc.hello;
+
+/**
+ * @Author Lc
+ * @Date 2023/3/15
+ * @Description
+ */
+public class getTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
